b8f892546cd08a918c730c24c606cbf67e941b75
Vadim Mostovenko <mostovenko.vadim@wb.ru>
2024-06-16 13:02:17 +0300

    delete excessive useShallow

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,14 +3,10 @@ import { useTodoStore } from "../store/useTodoStore";
 import { TodoItem } from "./TodoItem";
 import styles from "./styles.module.css";
 import { useGetTodosQuery } from "../api/todoApi";
-import { useShallow } from "zustand/react/shallow";
 
 export const TodoList: React.FC = () => {
 	const { data: todosFromApi } = useGetTodosQuery();
-
-	const todos = useTodoStore(useShallow((state) => state.todos));
-
-	const { setTodos } = useTodoStore();
+	const { setTodos, todos } = useTodoStore();
 
 	useEffect(() => {
 		if (todosFromApi) {
