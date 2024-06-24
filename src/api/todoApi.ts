fbd202bc018fb789d095bc7191e6e168098c3874
lamo2k123 <lamo2k123@gmail.com>
2024-06-24 19:24:14 +0300

    example

--- src/api/todoApi.ts
+++ src/api/todoApi.ts
@@ -4,11 +4,9 @@ import { Todo } from "../types/todo";
 export const todoApi = createApi({
 	reducerPath: "todoApi",
 	baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
-	tagTypes: ['todos'],
 	endpoints: (builder) => ({
 		getTodos: builder.query<Todo[], void>({
-			query		: () => "todos",
-			providesTags: ['todos']
+			query: () => "todos",
 		}),
 		addTodo: builder.mutation<Todo, Partial<Todo>>({
 			query: (newTodo) => ({
@@ -16,7 +14,35 @@ export const todoApi = createApi({
 				method: "POST",
 				body: newTodo
 			}),
-			invalidatesTags: ['todos']
+			async onQueryStarted(payload, { dispatch, queryFulfilled }) {
+				try {
+					const { data } = await queryFulfilled
+
+					/*
+					// Spread style
+					dispatch(
+						todoApi.util.updateQueryData('getTodos', undefined, (draft) => {
+							return [...draft, data];
+						})
+					);
+
+					// Push style
+					dispatch(
+						todoApi.util.updateQueryData('getTodos', undefined, (draft) => {
+							draft.push(data);
+						})
+					);
+					*/
+
+					dispatch(
+						todoApi.util.updateQueryData('getTodos', undefined, (draft) => {
+							return [...draft, data];
+						})
+					);
+				} catch {
+					// @TODO: Return error
+				}
+			},
 		}),
 		updateTodo: builder.mutation<Todo, Partial<Todo>>({
 			query: ({ id, ...patch }) => ({
@@ -24,14 +50,81 @@ export const todoApi = createApi({
 				method: "PUT",
 				body: patch,
 			}),
-			invalidatesTags: ['todos']
+			async onQueryStarted({ id, ...params }, { dispatch, queryFulfilled }) {
+				try {
+					await queryFulfilled
+
+					/*
+					// Map style
+					dispatch(
+						todoApi.util.updateQueryData('getTodos', undefined, (draft) => {
+							return draft.map((todo) => todo.id === id ? { ...todo, ...params } : todo)
+						})
+					);
+
+					// Index style
+					dispatch(
+						todoApi.util.updateQueryData('getTodos', undefined, (draft) => {
+							const index = draft.findIndex((todo) => todo.id === id);
+
+							if(index !== -1) {
+								draft[index] = {
+									...draft[index],
+									...params
+								};
+							}
+						})
+					);
+					*/
+
+					dispatch(
+						todoApi.util.updateQueryData('getTodos', undefined, (draft) => {
+							return draft.map((todo) => todo.id === id ? { ...todo, ...params } : todo)
+						})
+					);
+				} catch {
+					// @TODO: Return error
+				}
+			},
 		}),
 		deleteTodo: builder.mutation<void, string>({
 			query: (id) => ({
 				url: `todos/${id}`,
 				method: "DELETE",
 			}),
-			invalidatesTags: ['todos']
+			async onQueryStarted(id, { dispatch, queryFulfilled }) {
+				try {
+					await queryFulfilled
+
+					/*
+					// Filter style
+					dispatch(
+						todoApi.util.updateQueryData('getTodos', undefined, (draft) => {
+							return draft.filter((todo) => todo.id !== id);
+						})
+					);
+
+					// Index style
+					dispatch(
+						todoApi.util.updateQueryData('getTodos', undefined, (draft) => {
+							const index = draft.findIndex((todo) => todo.id === id);
+
+							if(index !== -1) {
+								draft.splice(index, 1);
+							}
+						})
+					);
+					*/
+
+					dispatch(
+						todoApi.util.updateQueryData('getTodos', undefined, (draft) => {
+							return draft.filter((todo) => todo.id !== id);
+						})
+					);
+				} catch {
+					// @TODO: Return error
+				}
+			},
 		}),
 	}),
 });
